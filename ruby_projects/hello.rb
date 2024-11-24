241b2bba7f623e771315fc51c3a55c7d9dbaf6bb
pauloskaterock <pauloengenharia5@yahoo.com>
2024-11-24 11:21:55 -0300

    Introduction

--- ruby_projects/hello.rb
+++ ruby_projects/hello.rb
@@ -1,12 +1,20 @@
-puts "hello word"
+# puts "hello word"
 
-p "hello Ruby"
+# p "hello Ruby"
 
-###############################
+# ###############################
 
-def say_hello
+# def say_hello
 
-    puts "hhello metodo"
+#     puts "hhello metodo"
+# end
+
+# say_hello
+
+#------------------------------------------------
+def say_hello(thing_to_say)
+    puts "thing_to_say"
+
 end
 
-say_hello
+say_hello "Hello PH"
